fe4544557da1577141c390e7c7beb32e52ffc2e0
amandalanay <amandalanay2@gmail.com>
2024-03-27 21:04:57 -0300

    adicione entrada de usuario

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog(null, "Digite seu nome");
+        JOptionPane.showMessageDialog(null, "meu nome é:" + nome);
     }
 }
